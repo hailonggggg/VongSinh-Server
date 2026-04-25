a9d5db1981d01866c8bee627c6e0a23f3e828764
long <hailongblbp1@gmail.com>
2026-04-25 15:20:39 +0700

    Update ServerNetwork.cs

--- Assets/Script/ServerNetwork.cs
+++ Assets/Script/ServerNetwork.cs
@@ -8,8 +8,6 @@ using UnityEngine;
 [RequireComponent(typeof(NetworkSceneManagerDefault))]
 public class ServerNetwork : MonoBehaviour, INetworkRunnerCallbacks
 {
-    [SerializeField]
-    private int loginSceneIndex = 2;
     public static ServerNetwork Instance { get; private set; }
     public event Action<PlayerRef, ArraySegment<byte>> OnReceiveNetworkData;
 
@@ -60,6 +58,7 @@ public class ServerNetwork : MonoBehaviour, INetworkRunnerCallbacks
     }
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
+        
         ClientManager.AddClient(new Client(player));
     }
 
@@ -179,7 +178,7 @@ public class ServerNetwork : MonoBehaviour, INetworkRunnerCallbacks
             GameMode = GameMode.Server,
             SessionName = "DedicatedServer",
             SceneManager = GetComponent<NetworkSceneManagerDefault>(),
-            Scene = SceneRef.FromIndex(loginSceneIndex)
+            // Scene = SceneRef.FromIndex(loginSceneIndex)
         });
 
         return networkRunner;
